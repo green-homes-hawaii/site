a8c96278203f5389c8ce58d3ae13542274a4563b
awitherow <au.witherow@gmail.com>
2019-01-29 13:19:33 -1000

    base intro

---
+++ js/template.js
@@ -0,0 +1,28 @@
+(function renderHeader() {
+	var links = [
+		{ href: './index.html', text: 'Home' },
+		{ href: './pages/buy.html', text: 'Buy' },
+		{ href: './pages/rent.html', text: 'Rent' },
+		{ href: './pages/consult.html', text: 'Services & Contact' },
+	];
+
+	document.getElementById('header-links').innerHTML =
+		"<ul class='navbar-nav ml-auto>'" +
+		links.map(function(link) {
+			renderHeaderLink(link);
+		}) +
+		'</ul>';
+})();
+
+function renderHeaderLink(link) {
+	var active = window.location.pathname.includes(link.text);
+	return (
+		"<li class='nav-item " +
+		active +
+		"'><a class='nav-link' href='" +
+		link.href +
+		"'>" +
+		link.text +
+		'/a></li>'
+	);
+}
